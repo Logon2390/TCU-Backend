2671bc180a082735352fdb509ce6c8208aa8dea7
Logon2390 <jorgeluis7040@gmail.com>
2025-06-12 16:23:38 -0600

    Add Docker support with Dockerfile, docker-compose.yml, and .dockerignore

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
 
   app.enableCors({
     origin: 'http://localhost:5173',
-    credentials: true, // para permitir cookies
+    credentials: true,
   });
   await app.listen(process.env.PORT ?? 3000);
 
