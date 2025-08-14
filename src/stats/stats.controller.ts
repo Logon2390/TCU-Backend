94016512a646f6c9a07810f618047c0fac12d752
JALLMAR ANDRES SAMUELS TORRES <jallmar.samuels@ucr.ac.cr>
2025-08-13 22:30:38 -0600

    Controller de stats

--- src/stats/stats.controller.ts
+++ src/stats/stats.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Put } from '@nestjs/common';
 import { StatsService } from './stats.service';
 import { CreateStatsDto } from './dto/create-stats.dto';
 import { UpdateStatsDto } from './dto/update-stats.dto';
@@ -11,4 +11,66 @@ import { ResponseDTO } from '../common/dto/response.dto';
 @Controller('stats')
 export class StatsController {
 
+    constructor(private readonly statsService: StatsService) { }
+
+    //endpoint para actualizar una estadistica
+    @Put(': id')
+    async update(@Param('id') id: string, @Body() dto: UpdateStatsDto) {
+        try {
+            const updatedStats = await this.statsService.update(+id, dto);
+            return new ResponseDTO(true, "Estadisticas actualizadas correctamente", updatedStats);
+        } catch (error) {
+            throw new Error(`Failed to update stats: ${error.message}`);
+        }
+    }
+
+    //endpoint para obtener todas las estadisticas
+    @Get()
+    @UseGuards(JwtAuthGuard, RolesGuard)
+    @Roles('M')
+    async findAll() {
+        try {
+            const stats = await this.statsService.findAll();
+            return new ResponseDTO(true, "Estadisticas obtenidas correctamente", stats);
+        } catch (error) {
+            throw new ResponseDTO(false, error.message);
+        }
+    }
+
+    //endpoint para obtener una estadistica
+    @Get(':id')
+    async findOne(@Param('id') id: string) {
+        try {
+            const stat = await this.statsService.findOne(+id);
+            if (!stat) return new ResponseDTO(false, "Estadistica no encontrada");
+            return new ResponseDTO(true, "Estadistica obtenida correctamente", stat);
+        } catch (error) {
+            throw new ResponseDTO(false, error.message);
+        }
+    }
+
+    //endpoint para crear una estadistica
+    @Post()
+    async create(@Body() dto: CreateStatsDto) {
+        try {
+            const newStat = await this.statsService.create(dto);
+            return new ResponseDTO(true, "Estadistica creada correctamente", newStat);
+        } catch (error) {
+            throw new ResponseDTO(false, error.message);
+        }
+    }
+
+    //endpoint para eliminar una estadistica
+    @Delete(':id')
+    @UseGuards(JwtAuthGuard, RolesGuard)
+    @Roles('M')
+    async remove(@Param('id') id: string) {
+        try {
+            const deletedStat = await this.statsService.remove(+id);
+            if (!deletedStat) return new ResponseDTO(false, "Estadistica no encontrada");
+            return new ResponseDTO(true, "Estadistica eliminada correctamente", deletedStat);
+        } catch (error) {
+            throw new ResponseDTO(false, error.message);
+        }
+    }
 }
