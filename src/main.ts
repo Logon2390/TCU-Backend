29bef30deb9b321ea3dd745faa0c574ac41598d0
Logon2390 <jorgeluis7040@gmail.com>
2025-08-24 20:09:55 -0600

    Update CORS configuration to allow requests from the Vercel frontend URL

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   app.use(cookieParser());
 
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost:5173', 'https://tcu-frontend-iota.vercel.app'],
     credentials: true,
   });
   await app.listen(process.env.PORT ?? 3000);
