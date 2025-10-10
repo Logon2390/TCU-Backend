c6edc145123dd5bc8ed95a8d458c489fcb3e8dfb
Logon2390 <jorgeluis7040@gmail.com>
2025-10-10 01:50:03 -0600

    Refactor main.ts to streamline application bootstrap process. Removed server instance caching and integrated request handling directly within the bootstrap function. Enhanced CORS configuration for improved security and compatibility.

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import cookieParser from 'cookie-parser';
-import { Server } from 'http';
 
-let server: Server;
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.use(cookieParser());
 
-export default async function handler(req: any, res: any) {
-  if (!server) {
-    const app = await NestFactory.create(AppModule);
-    app.use(cookieParser());
+  app.enableCors({
+    origin: ['http://localhost:5173', 'https://tcu-frontend-iota.vercel.app'],
+    credentials: true,
+  });
+  await app.listen(process.env.PORT ?? 3000);
 
-    app.enableCors({
-      origin: ['http://localhost:5173', 'https://tcu-frontend-iota.vercel.app'],
-      credentials: true,
-    });
-
-    await app.init();
-    server = app.getHttpAdapter().getInstance();
-  }
-
-  server.emit('request', req, res);
 }
+bootstrap();
