75fa66e864771a42ab13ca556fb6816fe3ac46f8
Logon2390 <jorgeluis7040@gmail.com>
2026-01-04 17:34:18 -0600

    Integrate helmet for enhanced security and implement global validation pipes in main.ts for improved request validation.

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,23 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import cookieParser from 'cookie-parser';
 import { env } from 'process';
+import { ValidationPipe } from '@nestjs/common';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
+  app.use(helmet());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
 
   app.enableCors({
-    origin: [
-      env.FRONTEND_URL,
-    ],
+    origin: [env.FRONTEND_URL],
     credentials: true,
   });
   await app.listen(process.env.PORT ?? 3000);
